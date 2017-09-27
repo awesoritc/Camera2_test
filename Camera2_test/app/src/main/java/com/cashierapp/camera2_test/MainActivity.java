29be440ad946b741c986a9c9f92a9aeeba2e2718
森松琢弥 <awesoritc@gmail.com>
2017-09-27 17:22:20 +0900

    外部ファイルに出力

--- Camera2_test/app/src/main/java/com/cashierapp/camera2_test/MainActivity.java
+++ Camera2_test/app/src/main/java/com/cashierapp/camera2_test/MainActivity.java
@@ -7,36 +7,12 @@ import android.view.TextureView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextureView.SurfaceTextureListener mSurfaceTextureListener = new TextureView.SurfaceTextureListener() {
-        @Override
-        public void onSurfaceTextureAvailable(SurfaceTexture surfaceTexture, int i, int i1) {
-
-        }
-
-        @Override
-        public void onSurfaceTextureSizeChanged(SurfaceTexture surfaceTexture, int i, int i1) {
-
-        }
-
-        @Override
-        public boolean onSurfaceTextureDestroyed(SurfaceTexture surfaceTexture) {
-            return false;
-        }
-
-        @Override
-        public void onSurfaceTextureUpdated(SurfaceTexture surfaceTexture) {
-
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextureView mTextureView = (TextureView) findViewById(R.id.camera2_texture);
-        if(mTextureView.isAvailable()){
-            mTextureView.setSurfaceTextureListener(mSurfaceTextureListener);
-        }
+
     }
 }
